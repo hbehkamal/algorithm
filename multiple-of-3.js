12e3117fce8dbe6140693dfa771e43ae70700307
Hosein BehkamaL <hoseynbehkamal@gmail.com>
2020-05-14 07:33:21 +0430

    Create multiple-of-3.js
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
    Find the sum of all the multiples of 3 or 5 below 1000.
    
    URL:  https://projecteuler.net/problem=1
    #projecteuler 
    #javascript

---
+++ multiple-of-3.js
@@ -0,0 +1,10 @@
+const fn = () => {
+    let x = 0;
+    for (let i = 1; i < 1000; i++) {
+        if ((i % 3 === 0) || (i % 5 === 0)) {
+            x += i;
+        }
+    }
+    return x;
+}
+console.log('---->', fn());
